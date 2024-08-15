6e680b8e9e233d8570365b0b0d695d4609746c32
Aleksandr Voloshin <cawa.voloshin@gmail.com>
2024-08-15 23:29:05 +0300

    feedback form

--- mehiko_motors/src/components/Feedback/Feedback.jsx
+++ mehiko_motors/src/components/Feedback/Feedback.jsx
@@ -4,8 +4,15 @@ import './Feedback.css'
 export default function Feedback() {
   return (
     <div className='feedback-wrapper'>
-      <span className='title'>Оставьте заявку на обратную связь</span>
-      <input type="text" />
+      <span className='title'>Хотите расчитать стоимость определённой машины?</span>
+      <span className='subtitle'>Оставьте заявку, и мы обязательно поможем вам.</span>
+      <div className='feedback-form'>
+        <form action="">
+          <input type="text" placeholder='ФИО'/> <br />
+          <input  type="tel" placeholder='+7 (900) 000-00-00'/> <br />
+          <input type="text" placeholder='BMW X6, 3 поколение'/>
+        </form>
+      </div>
     </div>
   )
 }
